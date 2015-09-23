4f22b1bd4f7a0b0450f3d5d7075ea427d5204a26
Jason Tyler <jmtyler@gmail.com>
2015-09-23 14:06:38 -0700

    at start of 'how to retrieve results in a particular order'

--- parallelism-1/main.py
+++ parallelism-1/main.py
@@ -1,4 +1,5 @@
-# Much of the code follows after http://sebastianraschka.com/Articles/2014_multiprocessing_intro.html
+# Much of the code follows verbatim after
+# http://sebastianraschka.com/Articles/2014_multiprocessing_intro.html
 # Please refer to Sebastian's terms of license; the MIT license might not apply to this code
 
 import multiprocessing as mp
@@ -6,3 +7,30 @@ import random
 import string
 
 # Define an output queue
+output = mp.Queue()
+
+# define a example function
+def rand_string(length, output):
+    """Generate a random string of numbers, lower- and uppercase chars"""
+    rand_str = ''.join(random.choice(
+                   string.ascii_lowercase
+                   + string.ascii_uppercase
+                   + string.digits)
+                for i in range(length))
+    output.put(rand_str)
+
+# Setup a list of processes that we want to run
+processes = [mp.Process(target=rand_string, args=(5, output)) for x in range(5)]
+
+# Running each process
+for p in processes:
+    p.start()
+
+# Exiting each completed process
+for p in processes:
+    p.join()
+
+#
+results = [output.get() for p in processes]
+
+print(results)
