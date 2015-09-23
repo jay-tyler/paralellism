4d94969a75648678a9b360e33e39505ee6302574
Jason Tyler <jmtyler@gmail.com>
2015-09-23 15:37:03 -0700

    about to start on benchmarking

---
+++ parallelism-1/part2.py
@@ -0,0 +1,18 @@
+# Much of the code follows verbatim after
+# http://sebastianraschka.com/Articles/2014_multiprocessing_intro.html
+# Please refer to Sebastian's terms of license; the MIT license might not apply to this code
+
+import multiprocessing as mp
+
+def cube(x):
+    return x ** 3
+
+# 1. apply() factorization
+pool = mp.Pool(processes=4)
+results = [pool.apply(cube, args=(x,)) for x in range(1,7)]
+print(results)
+
+# 2. map() factorization; equivalent to 1
+pool = mp.Pool(processes=4)
+results = pool.map(cube, range(1, 7))
+print(results)
